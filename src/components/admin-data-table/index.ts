dd745b9a009c9d742c04b784c8286c25cf9c50a6
Hamidreza Pakpour <136631329+Hamrez95@users.noreply.github.com>
2026-08-13 22:57:50 +0330

    feat(platform): export admin data table primitive

---
+++ src/components/admin-data-table/index.ts
@@ -0,0 +1,15 @@
+export {
+  AdminDataTable,
+  type AdminDataTableProps,
+  type AdminTableColumn,
+  type AdminTableState,
+} from "./admin-data-table";
+
+export {
+  DEFAULT_PAGE_SIZE,
+  MAX_PAGE_SIZE,
+  getTotalPages,
+  parseTableQuery,
+  toTableSearchParams,
+  type TableQueryState,
+} from "./table-query";
