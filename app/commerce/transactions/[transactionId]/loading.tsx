a91b415af5c6afc7d80080f6596b5470eb5e6820
Hamidreza Pakpour <136631329+Hamrez95@users.noreply.github.com>
2026-08-15 14:24:47 +0330

    feat(commerce): add transaction detail loading state

---
+++ app/commerce/transactions/[transactionId]/loading.tsx
@@ -0,0 +1,5 @@
+import { AdminPageState } from "@/src/components/admin-data-table";
+
+export default function CommerceTransactionDetailLoading() {
+  return <AdminPageState state="loading" title="در حال دریافت جزئیات تراکنش" />;
+}
